24634ded36211be900cff30164b0a71fe4fe8926
张锦江 <33757152@qq.com>
2018-03-07 16:49:36 +0800

    Initial Commit

---
+++ PullDrawerEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PullDrawerEffect
+//
+//  Created by 张锦江 on 2018/3/7.
+//  Copyright © 2018年 ZJJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
